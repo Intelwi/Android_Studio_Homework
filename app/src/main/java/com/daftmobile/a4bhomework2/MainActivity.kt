d2d63760f83c278a6858b7aa415081c64b26e450
Micha <mlk04@wp.pl>
2018-11-20 21:08:44 +0100

    finished

--- app/src/main/java/com/daftmobile/a4bhomework2/MainActivity.kt
+++ app/src/main/java/com/daftmobile/a4bhomework2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.daftmobile.a4bhomework2
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.content.res.Configuration
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
@@ -11,9 +12,7 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    fun create(){
         val button1 = findViewById<Button>(R.id.button1)
         val button2 = findViewById<Button>(R.id.button2)
         val button3 = findViewById<Button>(R.id.button3)
@@ -35,7 +34,27 @@ class MainActivity : AppCompatActivity() {
             dialog.setSignColor("blue")
             dialog.show(fm,"cos")
         }
+    }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        create()
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                setContentView(R.layout.activity_main)
+                create()
+            }
+            else if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                setContentView(R.layout.landscape)
+                create()
+            }
+        }
     }
 }
 
@@ -45,6 +64,7 @@ class DialogElem : DialogFragment() {
         fun setSignColor(color: String){
             this.color = color
         }
+
         override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
             return activity?.let {
                 // Use the Builder class for convenient dialog construction
